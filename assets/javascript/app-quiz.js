f2e572ac5462ccb00a3d87d901f750e92eb6fa6b
smendez92 <samuelremndez14@gmail.com>
2017-06-27 00:31:19 -0500

    main function done, working on objects

--- assets/javascript/app-quiz.js
+++ assets/javascript/app-quiz.js
@@ -214,126 +214,163 @@ var questionsObject = {
 
 };
 
-//Array of personalities
-
+//Array of personality objects with information to display on the results page and seed API calls
 var personalitiesArray = [
 
 	{
 		key: "Q1aAQ2aAQ3aAQ4aA",
-		name: "",
-		description: ""
-
+		name: "The Mover",
+		description: "The Mover has a spark for life and a passion for action & adventure they can barely contain — ",
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aAQ3aAQ4aB",
-		name: "",
-		description: ""
-
+		name: "The Trendsetter",
+		description: "The Trendsetter is the life of the party and the center of attention wherever they go — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aAQ3aAQ4aB",
-		name: "",
-		description: ""
-
+		name: "The Creator",
+		description: "The Creator has a mind of their own and a penchant for the arts — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aAQ3aBQ4bB",
-		name: "",
-		description: ""
-
+		name: "The Labrador Retriever",
+		description: "The Labrador Retriever loves their friends, their family, and sunny days — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aBQ3bAQ4cA",
-		name: "",
-		description: ""
-
+		name: "The Peacekeeper",
+		description: "The Peacekeeper description"
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aBQ3bAQ4cB",
-		name: "",
-		description: ""
-
+		name: "The Entertainer",
+		description: "Friendly, warm, and inviting, the Entertainer's home is your home — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aBQ3bBQ4dA",
-		name: "",
-		description: ""
-
+		name: "The Nester",
+		description: "The Nester — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aAQ2aBQ3bBQ4dB",
-		name: "",
-		description: ""
-
+		name: "The Inner Child",
+		description: " The Inner Child — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bAQ3cAQ4eA",
-		name: "",
-		description: ""
-
+		name: "The Nomad",
+		description: "The Nomad's heart is in many places at once — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bAQ3cAQ4eB",
-		name: "",
-		description: ""
-
+		name: "The Executive",
+		description: "The Executive is always planning their next big move— "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bAQ3cBQ4fA",
-		name: "",
-		description: ,""
-
+		name: "The Butterfly",
+		description: "The Butterfly — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bAQ3cBQ4fB",
-		name: "",
-		description: ""
-
+		name: "The Monarch",
+		description: "The Monarch — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bBQ3dAQ4gA",
-		name: "",
-		description: ""
-
+		name: "The Magician",
+		description: "The Magician can make anything possible with the right tools— "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bBQ3dAQ4gA",
-		name: "",
-		description: ""
-
+		name: "The Feline",
+		description: "The Feline knows how to unwind with a good stretch, a good nap, and a good snack — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bBQ3dBQ4hA",
-		name: "",
-		description: ""
-
+		name: "The Hermit",
+		description: "The Hermit enjoys the peace and quiet of a good night in— "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 	{
 		key: "Q1aBQ2bBQ3dBQ4hB",
-		name: "",
-		description: ""
-
+		name: "The Nurturer",
+		description: "The Nurturer — "
+		amazonKeyword: "",
+		ebayKeyword: "",
+		etsyKeyword: ""
 	},
 
 ];
 
 //Global variable that will hold our current user's personality ID, which will correspond to a specific API call to display a curated set of gifts
 var userPersonalityKey = "";
+
+//Global variable that will hold our user's final personality assignment by storing the key string, unique to each object in our personalitiesArray 
+var userPersonalityAssignment;
+
+//
+var currentQuestionNumber = 0;
 
 //function that takes in a questionID string, displays the corresponding text on the html page, and changes the corresponding html elements' data attribute
 function nextQuestion(questionId){
@@ -357,6 +394,7 @@ function nextQuestion(questionId){
 
 //User sees question 1
 nextQuestion("q1a");
+currentQuestionNumber++;
 
 
 $(document).on("click", ".JS-answer-choice", function(){
@@ -372,183 +410,197 @@ $(document).on("click", ".JS-answer-choice", function(){
 	if (tempAnswerId === "Q1aA"){
 
 		nextQuestion("q2a");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q1aB"){
+	else if (tempAnswerId === "Q1aB"){
 
 		nextQuestion("q2b");
+		currentQuestionNumber++;
 
 	}
 
 	//User sees Question 3
-	if (tempAnswerId === "Q2aA"){
+	else if (tempAnswerId === "Q2aA"){
 
 		nextQuestion("q3a");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q2aB"){
+	else if (tempAnswerId === "Q2aB"){
 
 		nextQuestion("q3b");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q2bA"){
+	else if (tempAnswerId === "Q2bA"){
 
 		nextQuestion("q3c");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q2bB"){
+	else if (tempAnswerId === "Q2bB"){
 
 		nextQuestion("q3d");
+		currentQuestionNumber++;
 
 	}
 
 	//USer sees Question 4
-	if (tempAnswerId === "Q3aA"){
+	else if (tempAnswerId === "Q3aA"){
 
 		nextQuestion("q4a");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3aB"){
+	else if (tempAnswerId === "Q3aB"){
 
 		nextQuestion("q4b");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3bA"){
+	else if (tempAnswerId === "Q3bA"){
 
 		nextQuestion("q4c");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3bB"){
+	else if (tempAnswerId === "Q3bB"){
 
 		nextQuestion("q4d");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3cA"){
+	else if (tempAnswerId === "Q3cA"){
 
 		nextQuestion("q4e");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3cB"){
+	else if (tempAnswerId === "Q3cB"){
 
 		nextQuestion("q4f");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3dA"){
+	else if (tempAnswerId === "Q3dA"){
 
 		nextQuestion("q4g");
+		currentQuestionNumber++;
 
 	}
 
-	if (tempAnswerId === "Q3dB"){
+	else if (tempAnswerId === "Q3dB"){
 
 		nextQuestion("q4h");
+		currentQuestionNumber++;
 
 	}
 
 	//User sees their final personality page
-	if (tempAnswerId === "Q4aA"){
+	if (tempAnswerId === "Q4aA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[0];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4aB"){
+	if (tempAnswerId === "Q4aB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[1];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4bA"){
+	if (tempAnswerId === "Q4bA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[2];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4bB"){
+	if (tempAnswerId === "Q4bB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[3];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4cA"){
+	if (tempAnswerId === "Q4cA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[4];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4cB"){
+	if (tempAnswerId === "Q4cB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[5];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4dA"){
+	if (tempAnswerId === "Q4dA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[6];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4dB"){
+	if (tempAnswerId === "Q4dB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[7];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4eA"){
+	if (tempAnswerId === "Q4eA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
+		userPersonalityAssignment = personalitiesArray[8];
 	}
 
-	if (tempAnswerId === "Q4eB"){
+	if (tempAnswerId === "Q4eB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[9];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4fA"){
+	if (tempAnswerId === "Q4fA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[10];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4fB"){
+	if (tempAnswerId === "Q4fB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[11];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4gA"){
+	if (tempAnswerId === "Q4gA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[12];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4gB"){
+	if (tempAnswerId === "Q4gB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[13];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4hA"){
+	if (tempAnswerId === "Q4hA" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[14];
+		currentQuestionNumber = 0;
 	}
 
-	if (tempAnswerId === "Q4hB"){
+	if (tempAnswerId === "Q4hB" && currentQuestionNumber === 4 ){
 
-		alert(personalityId);
-
+		userPersonalityAssignment = personalitiesArray[15];
+		currentQuestionNumber = 0;
 	}
 
 });
